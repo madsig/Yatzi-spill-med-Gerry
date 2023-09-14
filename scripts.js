79e28010d91c7a8bab7b78d154c7721329d43e76
madsig <madsig@getacademy.no>
2023-09-14 13:57:00 +0200

    Kan nå holde terninger

--- scripts.js
+++ scripts.js
@@ -1,22 +1,29 @@
 //model
-let roll;
+let roll = [];
 let dice = {1:'⚀', 2:'⚁', 3:'⚂', 4:'⚃', 5:'⚄', 6:'⚅'};
+let diceRolled = []
+let heldDice = [];
+let maxRollCount = 3;
+let timesRolled = 0;
 let frequencyTable = [];
+let firstLoad = false
+let selectedClass = [false, false, false, false, false]
 let player1Score = [null, null, null, null, null, null, null, null, null, 0] //1par, 2par, 3like, 4 like, liten straight, stor straight, hus, sjanse, yatzy
 let resultArray = {}
 const rowName = ['1 par', '2 par', '3 like', '4 like', 'Liten straight', 'Stor straight', 'Hus', 'Sjanse', 'Yatzy', 'Sum']
 const rowId = ['1pair', '2pair', '3same', '4same', 'SmallStraight', 'BigStraight', 'House', 'Chance', 'Yatzy']
+let diceBlocks = '';
+
+
 
 //view
 show();
 function show() {
-    roll = rollDice();
-    checkRoll();
-    let HTML = createTable();
-    HTML += `<h1>${displayDice(roll)}</h1>`
+    let HTML = `<div class="diceBlocks" >${displayDice(roll)}<button onclick="clickRollDice()">Kast terninger</button></div>`
+    HTML += createTable();
     document.getElementById('app').innerHTML = HTML;
 }
-
+    
 function createTable() {
     let table = /*HTML*/`
         <table>
@@ -45,7 +52,9 @@ function createTable() {
 function rollDice() {
     let rolls = [];
     for (let i = 0; i < 5; i++) {
+        if(!heldDice[i]){
         rolls.push(Math.floor(Math.random() * 6 + 1))
+        }else {rolls.push(heldDice[i])}
     }
     console.log(rolls)
     return rolls;
@@ -62,10 +71,13 @@ function checkRoll() {
 
 function getScore(id, index) {
     //Denne kan hente ut relevant score fra resultatArray, og oppdatere riktig variabel, så reloade view.
+    timesRolled = 0;
     if (player1Score[index] != null) return
     player1Score[index] = resultArray[id];
     resultArray['Sum'] = getSum2(player1Score);
     player1Score[9] = resultArray['Sum'];
+    roll = rollDice();
+    checkRoll();
     show();
 }
 
@@ -81,9 +93,34 @@ function getSum2(arr) {
 }
 
 function displayDice(arr) {
-    let diceBlocks = '';
-    for (let n of arr) {
-        diceBlocks += dice[n]
+    diceBlocks = '';
+    let idNr = 1;
+    for (let i = 0; i < arr.length; i++) {
+        diceBlocks += `<div ${selectedClass[i] ? 'class="selected"' : ''} onclick="clickDice(event)" id="die${i}">${dice[arr[i]]}</div>`
     }
+    
     return diceBlocks;
+}
+
+function clickDice(event) {
+    id = event.target.id.charAt(3);
+    
+    if (selectedClass[id] === false){
+        selectedClass[id] = true
+        heldDice[id] = roll[id];
+        diceHeld = heldDice.length;
+    } else {
+        selectedClass[id] = false
+        heldDice[id] = null;
+    }
+    console.log(heldDice);
+    show();
+}
+
+function clickRollDice() {
+    if (timesRolled >= 3 || selectedClass.every(element => element === true)) return;
+    timesRolled++;
+    roll = rollDice();
+    checkRoll();
+    show();
 }
